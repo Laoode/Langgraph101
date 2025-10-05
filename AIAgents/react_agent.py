af186dc988ceeffc71c24dcae563233cde9427dc
LM Yudhy Prayitno <119650090+Yudhy-code-stranger@users.noreply.github.com>
2025-10-05 09:21:57 +0800

    AIAgents/ feat: agent with tools

--- AIAgents/react_agent.py
+++ AIAgents/react_agent.py
@@ -2,9 +2,9 @@ from typing import Dict, TypedDict, Annotated, Sequence
 from langchain_core.messages import HumanMessage, AIMessage, BaseMessage, ToolMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode
 from langgraph.graph import StateGraph, END
 from langgraph.graph.message import add_messages
-from langgraph.prebuilt import ToolNode
 from dotenv import load_dotenv
 
 load_dotenv()
